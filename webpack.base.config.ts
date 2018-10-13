b8cec0e215955bfe76ba2559e84894ae0f7860e7
Hai Pham <swiften.svc@gmail.com>
2018-10-13 16:47:28 +0800

    feat(webpack): update entry path for dev to include __webpack_hmr

--- webpack.base.config.ts
+++ webpack.base.config.ts
@@ -1,14 +1,15 @@
 import * as DotEnv from 'dotenv-webpack';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
-import {JSObject, Objects} from 'javascriptutilities';
+import { JSObject, Objects } from 'javascriptutilities';
 import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import * as path from 'path';
 import * as UglifyJS from 'uglifyjs-webpack-plugin';
-import * as webpack from 'webpack';
 import {
   Configuration as Config,
+  DefinePlugin,
   HotModuleReplacementPlugin,
   Loader,
+  RuleSetRule
 } from 'webpack';
 
 export type Env = 'dev' | 'staging' | 'prod';
@@ -23,7 +24,8 @@ type ExtraConfig = Pick<Config, 'entry'> &
     rootLevelAliasPaths: JSObject<string>;
     dirName: string;
     publicPath?: string;
-    envFilePath?: string;
+    rootUrl?: string;
+    envVarsFilePath?: string;
     getEntryPaths: (dirName: string) => string | string[];
   }>;
 
@@ -35,41 +37,46 @@ export default function buildConfig(
   env: Env,
   extraConfigs: ExtraConfig
 ): Config {
-  let distFolder = 'dist_webpack';
-  let assetFolder = 'asset';
+  const { publicPath = '/', rootUrl = 'localhost:3000' } = extraConfigs;
+
+  const distFolder = 'dist_webpack';
+  const assetFolder = 'asset';
 
   return {
     ...Objects.deleteKeys(
       extraConfigs,
       'dirName',
-      'envFilePath',
+      'envVarsFilePath',
       'getEntryPaths',
       'publicPath',
+      'rootUrl',
       'rootLevelAliasPaths'
     ),
     entry: [
       ...((): string[] => {
         if (env === 'dev') {
-          return ['webpack-hot-middleware/client'];
-        } else {
-          return [];
+          return [
+            `webpack-hot-middleware/client?path=${publicPath}/${rootUrl}/__webpack_hmr&timeout=20000&reload=true`
+          ];
         }
+
+        return [];
       })(),
       ...(() => {
-        let extraPaths = extraConfigs.getEntryPaths(extraConfigs.dirName);
+        const extraPaths = extraConfigs.getEntryPaths(extraConfigs.dirName);
 
         if (extraPaths instanceof Array) {
           return extraPaths;
-        } else {
-          return [extraPaths];
         }
-      })(),
+
+        return [extraPaths];
+      })()
     ],
     output: {
+      publicPath,
       path: path.join(extraConfigs.dirName, distFolder),
       filename: '[name].bundle.js',
-      chunkFilename: '[name].bundle.js',
-      publicPath: extraConfigs.publicPath,
+      chunkFilename: '[name].bundle.js'
     },
     optimization: {
       splitChunks: {
@@ -77,42 +84,42 @@ export default function buildConfig(
           commons: {
             test: /[\\/]node_modules[\\/]/,
             name: 'vendor',
-            chunks: 'all',
-          },
-        },
-      },
+            chunks: 'all'
+          }
+        }
+      }
     },
     devtool: env === 'dev' ? 'source-map' : undefined,
     module: {
       rules: [
-        ...((): webpack.RuleSetRule[] => {
+        ...((): RuleSetRule[] => {
           return env === 'dev'
             ? [
                 {
                   enforce: 'pre',
                   test: /\.js$/,
                   loader: 'source-map-loader',
-                  exclude: [/node_modules/],
-                },
+                  exclude: [/node_modules/]
+                }
               ]
             : [];
         })(),
-        {test: /\.tsx?$/, loader: 'awesome-typescript-loader'},
-        {test: /\.html$/, exclude: /node_modules/, loader: 'html-loader'},
-        {test: /\.json$/, loader: 'json-loader'},
+        { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+        { test: /\.html$/, exclude: /node_modules/, loader: 'html-loader' },
+        { test: /\.json$/, loader: 'json-loader' },
         {
           test: /\.s?css?$/,
           use: [
             ((): Loader => {
               if (env === 'dev') {
                 return 'style-loader';
-              } else {
-                return MiniCssExtractPlugin.loader;
               }
+
+              return MiniCssExtractPlugin.loader;
             })(),
             'css-loader',
-            'sass-loader',
-          ],
+            'sass-loader'
+          ]
         },
         {
           test: /.*\.(gif|png|jpe?g)$/i,
@@ -126,45 +133,45 @@ export default function buildConfig(
                   assetFolder,
                   'images',
                   '[name]_[hash:7].[ext]'
-                ),
-              },
-            },
-          ],
-        },
-      ],
+                )
+              }
+            }
+          ]
+        }
+      ]
     },
     resolve: {
       alias: Objects.entries(extraConfigs.rootLevelAliasPaths)
-        .filter(([_key, value]) => value !== undefined && value !== null)
+        .filter(([_, value]) => value !== undefined && value !== null)
         .map(([key, value]) => ({
-          [key]: relativePath(extraConfigs.dirName, value!),
+          [key]: relativePath(extraConfigs.dirName, value!)
         }))
         .reduce((acc, v) => Object.assign(acc, v), {}),
-      extensions: ['.ts', '.tsx', '.js', '.json'],
+      extensions: ['.ts', '.tsx', '.js', '.json']
     },
     mode: env === 'dev' ? 'development' : 'production',
     node: {
       console: env === 'dev' ? undefined : false,
       fs: 'empty',
       net: 'empty',
-      tls: 'empty',
+      tls: 'empty'
     },
     plugins: [
-      new DotEnv({path: extraConfigs.envFilePath}),
+      new DotEnv({ path: extraConfigs.envVarsFilePath }),
       ...(() => (env === 'dev' ? [] : [new UglifyJS()]))(),
       ...(() => (env === 'dev' ? [new HotModuleReplacementPlugin()] : []))(),
       new MiniCssExtractPlugin({
         filename: '[name].[hash].css',
-        chunkFilename: '[id].[hash].css',
+        chunkFilename: '[id].[hash].css'
       }),
       new HtmlWebpackPlugin({
         template: `${extraConfigs.dirName}/src/index.html`,
         filename: 'index.html',
-        inject: 'body',
+        inject: 'body'
       }),
-      new webpack.DefinePlugin({
-        __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false')),
-      }),
-    ],
+      new DefinePlugin({
+        __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+      })
+    ]
   };
 }
